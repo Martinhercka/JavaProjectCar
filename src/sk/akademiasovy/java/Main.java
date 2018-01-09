0721eb2caba5dc23ccdf3e8ab5038aef5635000f
Martin Hercka <martin.hercka@akademiasovy.sk>
2018-01-09 11:35:01 +0100

    Added generate plate

--- src/sk/akademiasovy/java/Main.java
+++ src/sk/akademiasovy/java/Main.java
@@ -4,8 +4,10 @@ public class Main {
     public static void main(String[] args) {
         Car car1;
         Car car2;
+        Car car3;
         car1=new Car();
-        car2=new Car("Skoda 120L",6.5f);
+        car2=new Car("Skoda 120L",6.5f,"Presov");
+        car3=new Car("Seat Ibiza",7.1f,"Bratislava");
 
         car1.setBrand("Audi A8");
         car1.setGasConsumption(9.9f);
@@ -15,11 +17,31 @@ public class Main {
         car2.printInfo();
 
         car2.startEngine();
-        car2.increaseSpeed();
+        car2.increaseSpeed(80);
         System.out.println();
         car2.printInfo();
         System.out.println();
         System.out.println("Cost: KE---)Praha:"+car2.getPrice(1600,1.33f));
+        car1.startEngine();
+        car1.increaseSpeed(90);
+        car1.turnOnFrontWiper();
+        car1.printInfo();
+
+        System.out.println();
+
+        if (car2.getSpeed()>car1.getSpeed())
+            System.out.println("Skoda 120L is faster");
+        else if(car1.getSpeed()>car2.getSpeed())
+            System.out.println("Audi A8 is faster");
+        else if(car1.getSpeed()==car2.getSpeed())
+            System.out.println("Same speed");
+        car3.printInfo();
+
+
 
     }
+
+
+
+
 }
